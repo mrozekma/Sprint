3b13a45435fc0b1d6142eef66eeb6073b84a6bbb
Michael Mrozek <git@mrozekma.com>
2011-10-06 17:03:58 -0400

    Added 'styleChanges' argument to ProgressBar, and changed the sprint metrics page to use it

--- ProgressBar.py
+++ ProgressBar.py
@@ -2,10 +2,14 @@ from __future__ import division
 
 from rorn.ResponseWriter import ResponseWriter
 
+from utils import *
+
 inf = float('inf')
 
 class ProgressBar:
-	def __init__(self, label, amt, total, pcnt = None, zeroDivZero = True):
+	# zeroDivZero means the progress should be 0 if amt == total == 0. It will be 100 if False. In either case, it will be infinity if amt > 0
+	# styleChanges is a map of percentages to styles to apply to div.progress-current; the highest percentage <= this bar's percentage will be applied
+	def __init__(self, label, amt, total, pcnt = None, zeroDivZero = True, styleChanges = {}):
 		self.label = label
 		self.amt = amt
 		self.total = total
@@ -16,13 +20,16 @@ class ProgressBar:
 		else:
 			self.pcnt = amt / total * 100
 
+		topPcnt = maxOr(filter(lambda p: p <= self.pcnt, styleChanges.keys()))
+		self.cls = styleChanges[topPcnt] if topPcnt in styleChanges else None
+
 	def __str__(self):
 		w = ResponseWriter()
 		if self.label:
 			print "%s " % self.label
 		print "<div class=\"task-progress-total\" style=\"position: relative; top: 5px\">"
 		if self.pcnt > 0:
-			print "<div class=\"progress-current%s\" style=\"width: %d%%;\">" % (' progress-current-red' if self.pcnt > 100 else '', min(self.pcnt, 100))
+			print "<div class=\"progress-current%s\" style=\"width: %d%%;\">" % (" %s" % self.cls if self.cls else '', min(self.pcnt, 100))
 		print "<span class=\"progress-percentage\">%d/%d hours (%s%%)</span>" % (self.amt, self.total, '&#8734;' if self.pcnt == inf else "%d" % self.pcnt)
 		if self.pcnt > 0:
 			print "</div>"
