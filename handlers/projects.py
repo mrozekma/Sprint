2d7fd6d7d0661e1ea210334e41738e68afe6bab3
Michael Mrozek <git@mrozekma.com>
2011-11-03 21:31:07 -0400

    Show projects with active sprints the user is a member of first

--- handlers/projects.py
+++ handlers/projects.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 
+from rorn.ResponseWriter import ResponseWriter
 from rorn.Session import undelay
 from rorn.Box import ErrorBox
 
@@ -17,12 +18,16 @@ def projects(handler, request):
 
 def showProjects(handler):
 	undelay(handler)
-	print "<div class=\"indented\">"
+
+	boxes = {} # project -> {'str': box HTML, 'weight': ascending sort weight}
 	for project in Project.loadAll():
+		writer = ResponseWriter()
 		sprints = project.getSprints()
+		isActive = any(map(lambda sprint: sprint.isActive(), sprints))
+		isMember = False
 
 		classes = ['project-summary']
-		if any(map(lambda sprint: sprint.isActive(), sprints)):
+		if isActive:
 			classes.append('active')
 		print "<div id=\"project-summary-%d\" class=\"%s\">" % (project.id, ' '.join(classes))
 		print "<div class=\"project-name\">%s</div>" % project.name
@@ -35,6 +40,7 @@ def showProjects(handler):
 		print "<div class=\"project-members\">"
 		print "<div class=\"member scrummaster\">%s</div>" % project.owner.str('scrummaster')
 		for member in sorted(project.getMembers()):
+			if member == handler.session['user']: isMember = True
 			if member == project.owner: continue
 			print "<div class=\"member\">%s</div>" % member.str('member')
 		print "</div>"
@@ -49,4 +55,10 @@ def showProjects(handler):
 
 		print "<div class=\"clear\"></div>"
 		print "</div>"
+		boxes[project] = {'str': writer.done(), 'weight': 0 if isActive and isMember else 1}
+
+	print "<div class=\"indented\">"
+	# Show projects with active sprints this user is a member of first
+	for project in sorted(boxes.keys(), key = lambda p: boxes[p]['weight']):
+		print boxes[project]['str']
 	print "</div>"
