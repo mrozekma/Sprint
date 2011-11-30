ca2720a8254aeefacbc7531f4eb09882abe89b21
Michael Mrozek <git@mrozekma.com>
2011-11-30 10:45:53 -0500

    Fixed git revision info broken in 13d701bbf7901eb4e99f441e25d520db487c9fcf

--- LoadValues.py
+++ LoadValues.py
@@ -4,10 +4,11 @@ import os
 from datetime import datetime
 
 loadTime = datetime.now()
-revisionHash, revisionDate, revisionRelative = os.popen('git log -n 1 --format=format:"%H %ct %cr"').read().split(' ', 2)
-revisionDate = tsToDate(int(revisionDate)).strftime('%d %b %Y %H:%M:%S')
 
 def getRevisionInfo():
+	# These are recomputed each time because revisionRelative changes
+	revisionHash, revisionDate, revisionRelative = os.popen('git log -n 1 --format=format:"%H %ct %cr"').read().split(' ', 2)
+	revisionDate = tsToDate(int(revisionDate)).strftime('%d %b %Y %H:%M:%S')
 	return revisionHash, revisionDate, revisionRelative
 
 def getLoadtime():
