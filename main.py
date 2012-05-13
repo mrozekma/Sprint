336dab133cce9039a7173144f1cb9068260db79b
Michael Mrozek <git@mrozekma.com>
2012-05-13 01:19:21 -0400

    Sane socket errors

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from BaseHTTPServer import HTTPServer
 from HTTPHandler import HTTPHandler
+import socket
+
 from Cron import Cron
 from Settings import PORT
 from Update import check
 
 check()
 
-server = HTTPServer(('', PORT), HTTPHandler)
+try:
+	server = HTTPServer(('', PORT), HTTPHandler)
+except socket.error, (errno, msg):
+	print "Unable to open port %d: %s" % (PORT, msg)
+	exit(1)
+
 Cron.start()
+
 try:
 	server.serve_forever()
 except KeyboardInterrupt:
