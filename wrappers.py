58262e3e3d38c1a0e21dfa19aa96cc70265f102f
Michael Mrozek <git@mrozekma.com>
2012-05-23 02:02:19 -0400

    Removed old code

--- wrappers.py
+++ wrappers.py
@@ -11,25 +11,7 @@ from Settings import settings
 from User import User
 from utils import *
 
-# class LeftMenu:
-	# def __init__(self):
-		# self.entries = []
-
-	# # text => url
-	# def __setitem__(self, k, v):
-		# self.entries.append((k, v))
-
-	# def clear(self):
-		# self.entries = []
-
-	# def __str__(self):
-		# if len(self.entries) == 0:
-			# return "&nbsp;"
-
-		# return "<br>\n".join(["<a href=\"%s\">%s</a>" % (url, text) for (text, url) in self.entries])
-
 def header(handler, path):
-	# print "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">"
 	print "<!DOCTYPE html>"
 	print "<html>"
 	print "<head>"
@@ -49,7 +31,7 @@ def header(handler, path):
 	print "<link rel=\"stylesheet\" href=\"/static/chosen/chosen.css\" />"
 	print "<script src=\"/static/chosen/chosen.jquery.js\" type=\"text/javascript\"></script>"
 
-	print "<script src=\"/static/script.js?v=%d\" type=\"text/javascript\"></script>" % randint(0, 1000000) # TODO Remove
+	print "<script src=\"/static/script.js\" type=\"text/javascript\"></script>"
 
 	print "<style type=\"text/css\">"
 	print "#main_a {"
@@ -61,12 +43,6 @@ def header(handler, path):
 		print "    font-weight: bold;"
 		print "}"
 	print "</style>"
-
-		# print "<script type=\"text/javascript\">"
-		# print "$(document).ready(function () {"
-		# print "    $(\".username:econtains('%s')\").addClass('me');" % handler.session['user'].username
-		# print "});"
-		# print "</script>"
 
 	print "</head>"
 	print "<body>"
@@ -114,21 +90,13 @@ def header(handler, path):
 
 	print "<div id=\"main_d\">"
 
-	# print "<div style=\"width: 200px; float: left\">"
-	# print handler.leftMenu
-	# print "</div>"
-
-	# print "<div style=\"width: 700px; float: left\">"
 	print "<h1>$bodytitle$</h1>"
 
 def footer(handler, path):
-	# print "</div>"
 	print "</div>"
 	print "<br style=\"clear:both\">"
 	print "</div>"
 	print "</div>"
-
-	# print "<br style=\"clear:both\">"
 
 	print "</div>"
 
@@ -151,17 +119,3 @@ def footer(handler, path):
 
 	print "</body>"
 	print "</html>"
-
-# class Frame:
-	# def __init__(self, handler, path, title = None):
-		# self.handler = handler
-		# self.path = path
-		# self.title = title
-
-	# def __enter__(self):
-		# header(self.handler, self.path)
-		# if self.title:
-			# self.handler.title(self.title)
-
-	# def __exit__(self, *args):
-		# footer(self.handler, self.path)
