5fa2dec9a23c18aa042045d7064d5ac2cc0e4bf2
Michael Mrozek <git@mrozekma.com>
2012-06-19 15:03:17 -0400

    Structured event publishing

--- event_handlers/EventPublisher.py
+++ event_handlers/EventPublisher.py
@@ -1,53 +1,51 @@
 from redis import StrictRedis
 import re
+from json import dumps as toJS
 
 from Event import EventHandler
 from User import User
-
-def formatChannelName(projectName):
-	return re.sub('\W+', '_', projectName.lower())
 
 class EventPublisher(EventHandler):
 	def __init__(self, host, port):
 		self.conn = StrictRedis(host = host, port = port)
 		self.conn.pubsub()
 
-	def publish(self, sprint, message):
-		channel = formatChannelName(sprint.project.name)
+	def publish(self, handler, sprint, type, **extraArgs):
+		data = {'type': type, 'user': handler.session['user'].username}
+		data.update(extraArgs)
+
+		channel = EventPublisher.formatChannelName(sprint.project.name)
 		try:
-			self.conn.publish(channel, message)
+			self.conn.publish(channel, toJS(data))
 		except Exception:
 			pass
+
+	@staticmethod
+	def formatChannelName(projectName):
+		return re.sub('\W+', '_', projectName.lower())
 
 	# EventHandler interface
 
 	def newSprint(self, handler, sprint):
-		self.publish(sprint, "%s created a new sprint: %s" % (handler.session['user'].username, sprint.name))
+		self.publish(handler, sprint, 'newSprint', name = sprint.name)
 
 	def sprintInfoUpdate(self, handler, sprint, changes):
 		import sys
 		if changes['end']:
-			self.publish(sprint, "%s extended the sprint; it now ends %s" % (handler.session['user'].username, changes['end'].strftime('%A, %B %d')))
+			self.publish(handler, sprint, 'sprintExtend', end = changes['end'].strftime('%A, %B %d'))
 		if changes['addMembers']:
-			if len(changes['addMembers']) == 1:
-				self.publish(sprint, "%s added %s to the sprint" % (handler.session['user'].username, changes['addMembers'][0].username))
-			elif len(changes['addMembers']) == 2:
-				self.publish(sprint, "%s added %s and %s to the sprint" % (handler.session['user'].username, changes['addMembers'][0].username, changes['addMembers'][1].username))
-			else:
-				self.publish(sprint, "%s added %sand %s to the sprint" % (handler.session['user'].username, ''.join(user.username + ', ' for user in changes['addMembers'][:-1]), changes['addMembers'][-1].username))
+			self.publish(handler, sprint, 'addMembers', usernames = [user.username for user in changes['addMembers']])
 
 	def sprintAvailUpdate(self, handler, sprint):
-		self.publish(sprint, "%s updated sprint member availability" % handler.session['user'].username)
+		self.publish(handler, sprint, 'sprintAvailUpdate')
 
 	def newTask(self, handler, task):
-		self.publish(task.sprint, "%s created a new task: %s (assigned to %s, %d %s)" % (handler.session['user'].username, task.name, task.assigned.username, task.hours, 'hour' if task.hours == 1 else 'hours'))
+		self.publish(handler, task.sprint, 'newTask', name = task.name, assigned = task.assigned.username, hours = task.hours)
 
 	def taskUpdate(self, handler, task, field, value):
-		textValue = value
-		if isinstance(textValue, User):
-			textValue = textValue.username
-		#TODO Pull info from the History class
-		self.publish(task.sprint, "%s updated a task: %s" % (handler.session['user'].username, task.name))
+		if isinstance(value, User):
+			value = value.username
+		self.publish(handler, task.sprint, 'taskUpdate', id = task.id, name = task.name, field = field, value = value)
 
 	def newNote(self, handler, note):
-		self.publish(note.task.sprint, "%s created a new note on: %s" % (handler.session['user'].username, note.task.name))
+		self.publish(handler, note.task.sprint, 'newNote', taskName = note.task.name)
