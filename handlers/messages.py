a6254220be7461aa8d05ba9300476549568105b5
Michael Mrozek <git@mrozekma.com>
2012-08-11 14:50:23 -0400

    Add the ability to retract/delete messages

--- handlers/messages.py
+++ handlers/messages.py
@@ -6,6 +6,7 @@ from Message import Message
 from User import User
 from Tabs import Tabs
 from Markdown import Markdown
+from Button import Button
 from utils import *
 
 tabs = Tabs()
@@ -20,6 +21,7 @@ def inbox(handler, request):
 	print InfoBox("Note", "You can control your automated message subscriptions from the <a href=\"/prefs\">preferences</a> page")
 
 	print tabs << 'inbox'
+	undelay(handler)
 
 	Markdown.head('div.message .body pre code')
 
@@ -51,14 +53,20 @@ def printMessage(message, inbox):
 
 	user = (message.sender if inbox else message.user) or None
 
+	print "<form method=\"post\" action=\"/messages/%d/delete\">" % message.id
 	print "<div class=\"%s\">" % ' '.join(cls)
 	print "<div class=\"avatar\"><img src=\"%s\"></div>" % (user.getAvatar() if user else '/static/images/user-system.png')
 	print "<div class=\"title\">"
+	if inbox:
+		print "<input type=\"image\" src=\"/static/images/cross.png\" title=\"Delete message\">"
+	elif not message.read:
+		print "<input type=\"image\" src=\"/static/images/message-retract.png\" title=\"Retract message\">"
 	print "%s %s %s" % (message.title, 'by' if inbox else 'to', user or 'system')
 	print "<div class=\"timestamp\">%s</div>" % tsToDate(message.timestamp).replace(microsecond = 0)
 	print "</div>"
 	print "<div class=\"body markdown\">%s</div>" % message.render()
 	print "</div>"
+	print "</form>"
 
 
 @post('messages/send')
@@ -74,3 +82,27 @@ def send(handler, request, p_userid, p_body):
 	Message(target.id, handler.session['user'].id, "Direct message", p_body, 'markdown').save()
 	delay(handler, SuccessBox("Message dispatched to %s" % target, close = 3))
 	redirect('/messages/sent')
+
+@post('messages/(?P<id>[0-9]+)/delete')
+def deleteMessage(handler, request, id, p_x = None, p_y = None):
+	handler.title('Delete message')
+	requirePriv(handler, 'User')
+
+	id = int(id)
+	message = Message.load(id)
+	if not message:
+		ErrorBox.die("Message %d does not exist" % id)
+	if message.user == handler.session['user']:
+		verb = 'deleted'
+		redir = '/messages/inbox'
+	elif message.sender == handler.session['user']:
+		verb = 'retracted'
+		redir = '/messages/sent'
+		if message.read:
+			ErrorBox.die("Unable to retract read messages")
+	else:
+		ErrorBox.die("You don't have permission to delete this message")
+
+	message.delete()
+	delay(handler, SuccessBox("Message %s" % verb, close = 3))
+	redirect(redir)
